7285cd668a0362a430220582cdaec57403ac78a7
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2024-01-21 22:40:26 +0800

    updated homepage and plants page according to figma

---
+++ Data.js
@@ -0,0 +1,34 @@
+export let userData = {
+  plants: [
+    {
+      name: "Plant 1",
+      locked: false,
+      stats: {
+        HP: 200,
+        ATK: 20,
+        Range: 10,
+        level: 1,
+      },
+    },
+    {
+      name: "Plant 2",
+      locked: false,
+      stats: {
+        HP: 200,
+        ATK: 20,
+        Range: 10,
+        level: 1,
+      },
+    },
+    {
+      name: "Plant 2",
+      locked: false,
+      stats: {
+        HP: 200,
+        ATK: 20,
+        Range: 10,
+        level: 1,
+      },
+    },
+  ],
+};
--- Plants/index.js
+++ Plants/index.js
@@ -1,8 +1,5 @@
-import { database } from "../Firebase.js";
-import {
-  set,
-  ref,
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { userData } from "../Data.js";
+// let userData = JSON.parse(localStorage.getItem("userInfo"));
 
 const createPlantDisplay = (locked) => {
   let displayWrapper = document.createElement("div");
@@ -20,8 +17,6 @@ const createPlantDisplay = (locked) => {
 
 function displayGallery() {
   const plantWrapper = document.querySelector("#gallery-wrapper");
-  let userData = JSON.parse(localStorage.getItem("userInfo"));
-
   for (let i = 0; i < userData["plants"].length; i++) {
     let plantDiv = createPlantDisplay(userData["plants"][i].locked);
     plantWrapper.appendChild(plantDiv);
@@ -38,69 +33,50 @@ function displayGallery() {
 
 const createDetailsDisplay = (plant) => {
   const plantImg = document.querySelector("#plantImg");
-  const name = document.querySelector("#name");
+  const name = document.querySelectorAll(".name");
   const level = document.querySelector("#level");
-  const stats = document.querySelectorAll(".stat-row");
+  const stats = document.querySelectorAll(".stat-val");
 
   // plantImg.src = plant["img"];
-  name.textContent = plant["name"];
+  name[0].textContent = plant["name"]; // two labels
+  name[1].textContent = plant["name"];
   level.textContent = "Level " + plant["stats"]["level"];
-  stats[0].textContent = plant["stats"]["HP"];
-  stats[1].textContent = plant["stats"]["ATK"];
-  stats[2].textContent = plant["stats"]["Range"];
-  stats[3].textContent = plant["stats"]["ATK Speed"];
+
+  stats[0].children[1].textContent = plant["stats"]["HP"];
+  stats[1].children[1].textContent = plant["stats"]["ATK"];
+  stats[2].children[1].textContent = plant["stats"]["Range"];
 };
-
-function updateDetails(userData, stat, selectedIndex) {
-  userData["plants"][selectedIndex]["stats"][stat] += 1;
-  localStorage.setItem("userInfo", JSON.stringify(userData));
-
-  set(
-    ref(
-      database,
-      "users/" +
-        localStorage.getItem("SSTea-UID") +
-        "/plants/" +
-        selectedIndex +
-        "/stats/" +
-        stat
-    ),
-    userData["plants"][selectedIndex]["stats"][stat]
-  )
-    .then(() => {
-      createDetailsDisplay(userData["plants"][selectedIndex]);
-      alert("Stat upgraded!");
-    })
-    .catch((error) => {
-      console.log(error.code);
-    });
-}
 
 function displayDetails() {
   let selectedIndex = window.localStorage.getItem("plantIndex");
-  let userData = JSON.parse(localStorage.getItem("userInfo"));
   createDetailsDisplay(userData["plants"][selectedIndex]);
 
   const upgradeBtns = document.querySelectorAll(".upgrade-btn");
   const levelBtn = document.querySelector(".level-btn");
 
-  upgradeBtns[0].addEventListener("click", () => {
-    updateDetails(userData, "HP", selectedIndex);
-  });
-  upgradeBtns[1].addEventListener("click", () => {
-    updateDetails(userData, "ATK", selectedIndex);
-  });
-  upgradeBtns[2].addEventListener("click", () => {
-    updateDetails(userData, "Range", selectedIndex);
-  });
-  upgradeBtns[3].addEventListener("click", () => {
-    updateDetails(userData, "ATK Speed", selectedIndex);
-  });
+  for (let i = 0; i < upgradeBtns.length; ++i) {
+    upgradeBtns[i].addEventListener("click", () => {
+      displayLevelUp(upgradeBtns[i].getAttribute("stat"));
+    });
+  }
 
   levelBtn.addEventListener("click", () => {
     updateDetails(userData, "level", selectedIndex);
   });
 }
+
+const displayLevelUp = (stat) => {
+  const main = document.querySelector("#main");
+  const popUp = document.querySelector("#popup");
+  const closeBtn = document.querySelector(".close-btn");
+  main.classList.add("blur");
+  popUp.classList.add("visible");
+
+  closeBtn.addEventListener("click", () => {
+    main.classList.remove("blur");
+    popUp.classList.remove("visible");
+  });
+};
 
 if (window.location.pathname == "/Plants/index.html") {
   displayGallery();
