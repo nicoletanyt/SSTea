c913bf4abee38cfc256902815eeefbf656aa2f3b
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2024-01-27 12:34:24 +0800

    finished ui of selecting levels (battle) page

--- Battle/index.js
+++ Battle/index.js
@@ -1,12 +1,20 @@
 function selectLevels() {
+  const popup = document.getElementById("popup");
+  const main = document.getElementById("main");
   const levels = document.querySelectorAll(".level-label");
+  const closeBtn = document.querySelector(".close-btn");
 
   for (let i = 0; i < levels.length; i++) {
     levels[i].addEventListener("click", () => {
-      localStorage.setItem("level", i + 1);
-      window.location.pathname = "/Battle/selectCharacter.html";
+      localStorage.setItem("level", levels[i].textContent);
+      popup.classList.remove("hidden");
+      popup.classList.add("show");
+      main.classList.add("hidden");
     });
   }
+  closeBtn.addEventListener("click", () => {
+    window.location.pathname = "/Battle/selectCharacter.html";
+  });
 }
 
 function selectChar() {
