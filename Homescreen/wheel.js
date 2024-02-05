d2d37d40e4027ecba891f0520a8216c9c652eb78
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2024-02-05 11:29:01 +0800

    added the question popup

--- Homescreen/wheel.js
+++ Homescreen/wheel.js
@@ -18,12 +18,63 @@ for (let i = 0; i < sections; ++i) {
 circle.addEventListener("click", () => {
   console.log(number);
   circle.style.transform = "rotate(" + number + "deg)";
+  number += Math.ceil(Math.random() * 1000);
   // show alert after its done spinning. duration should be == transition
+  let index = parseInt(((number - deg) % 360) / deg) - 1;
+  if (index < 0) index = 0;
   setTimeout(() => {
-    alert(
-      "You won " + rewards[parseInt(((number - deg) % 360) / deg) - 1] + "!"
-    );
+    alert("You won " + rewards[index] + "!");
+    if (rewards[index] == "Quiz") {
+      console.log("quiz!");
+      showQuestion();
+    }
     // circle.style.transition = "0s";
     // circle.style.transform = "none";
   }, 5000);
 });
+
+function showQuestion() {
+  const main = document.getElementById("spin-wrapper");
+  const popup = document.getElementById("popup");
+  const options = document.querySelectorAll(".option");
+  const closeBtn = document.querySelector(".close-btn");
+  let userData = JSON.parse(localStorage.getItem("userInfo"));
+  let selected = false;
+
+  // Open Quiz Popup
+  main.classList.add("hide");
+  popup.classList.remove("hide");
+  popup.classList.add("show");
+
+  function selectOption(i) {
+    if (i == 1) {
+      // The second option is the correct one. If expanding more questions, use a dictionary instead.
+      alert("Correct! You get 3 leaf points.");
+      //   leaf.textContent = parseInt(leaf.textContent) + 3;
+      userData["currency"]["leaf"] += 3;
+      localStorage.setItem("userInfo", userData);
+    } else {
+      alert("Wrong.");
+    }
+
+    // set the background colour of all to red
+    for (let j = 0; j < options.length; ++j) {
+      options[j].style.backgroundColor = "#ECA595";
+    }
+    // set the background colour for option 1 to green
+    options[1].style.backgroundColor = "#B4EC95";
+    selected = true;
+  }
+
+  for (let i = 0; i < options.length; ++i) {
+    options[i].addEventListener("click", () => {
+      if (!selected) selectOption(i);
+    });
+  }
+
+  closeBtn.addEventListener("click", () => {
+    main.classList.remove("hide");
+    main.classList.add("show");
+    popup.classList.add("hide");
+  });
+}
--- Shop/index.js
+++ Shop/index.js
@@ -38,12 +38,6 @@ const items = [
     desc: "x10",
     cost: "x1",
   },
-  {
-    img: "../Assets/Homepage/question.png",
-    item: "leaf",
-    desc: "Quiz",
-    cost: "x3",
-  },
 ];
 
 for (let i = 0; i < items.length; ++i) {
@@ -89,49 +83,6 @@ for (let i = 0; i < items.length; ++i) {
       getValues();
     } else {
       alert("You do not have enough leaf points");
-    }
-
-    if (desc.textContent == "Quiz") {
-      // Open Quiz Popup
-      main.classList.add("hide");
-      popup.classList.remove("hide");
-      popup.classList.add("show");
-
-      const options = document.querySelectorAll(".option");
-      const closeBtn = document.querySelector(".close-btn");
-      let selected = false;
-
-      function selectOption(i) {
-        if (i == 1) {
-          // The second option is the correct one. If expanding more questions, use a dictionary instead.
-          alert("Correct! You get 3 leaf points.");
-          leaf.textContent = parseInt(leaf.textContent) + 3;
-          userData["currency"]["leaf"] += 3;
-          getValues();
-        } else {
-          alert("Wrong.");
-        }
-
-        // set the background colour of all to red
-        for (let j = 0; j < options.length; ++j) {
-          options[j].style.backgroundColor = "#ECA595";
-        }
-        // set the background colour for option 1 to green
-        options[1].style.backgroundColor = "#B4EC95";
-        selected = true;
-      }
-
-      for (let i = 0; i < options.length; ++i) {
-        options[i].addEventListener("click", () => {
-          if (!selected) selectOption(i);
-        });
-      }
-
-      closeBtn.addEventListener("click", () => {
-        main.classList.remove("hide");
-        main.classList.add("show");
-        popup.classList.add("hide");
-      });
     }
 
     // save to localStorage
